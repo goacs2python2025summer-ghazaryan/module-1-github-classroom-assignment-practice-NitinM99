16033c738bc1894bbadb96ab68c71001a7e49a76
NitinM99 <159864407+NitinM99@users.noreply.github.com>
2025-06-20 01:27:50 -0400

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print("I did lots of volunteering and research work, mainly in health, medicine and neuroscience, and actually found a correlation between 2 factors in neurological disease")
